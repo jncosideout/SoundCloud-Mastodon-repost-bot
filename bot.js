e0953d1dd44a58c01af4b27902a1ea01245c29be
Alex Beaty <macarrao_1@protonmail.ch>
2020-07-03 00:15:40 -0500

    added official soundcloud sdk

--- bot.js
+++ bot.js
@@ -1,32 +1,52 @@
 require('dotenv').config();
-console.log("Mastadon bot starting...");
-const Mastadon = require('mastodon-api');
-const fs = require('fs');
+var SC = require('soundcloud');
 
-const M = new Mastadon({
-    client_key: process.env.CLIENT_KEY,
-    client_secret: process.env.CLIENT_SECRET,
-    access_token: process.env.AUTH_TOKEN,
-    timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
-    api_url: 'https://botsin.space/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
+SC.initialize({
+    client_id: process.env.SC_CLIENT_ID,
+    oauth_token: process.env.SC_AUTH_TOKEN
 })
-
-const params = {
-    status: "this song came from  my feed on SC\n \
-    https://soundcloud.com/dannyplayamaqui/ham-ice-cream-a-meltdown \
-    follow me for more cool EDM tracks \
-    https://soundcloud.com/sour_cream_pringles "
-}
-
-M.post('statuses', params, (err, data, response) => {
-    if (err) {
-        console.log(err);
-    } else {
-        //reference for data output
-        //fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
-        console.log(data);
-        console.log(`ID: ${data.id} and timestamp: ${data.created_at}`);
-        console.log(data.content);
-        console.log(response);
-    }
+console.log("getting sourcreampringles soundcloud reposts")
+SC.put('/me',{
+    user: {description: 'I am using the SouncCloud API!'}
+}).then(function(){
+    return SC.get('/me');
+}).then(function(me){
+    console.log(me.description);
+}).catch(function(error){
+    console.log(error);
 });
+
+
+// console.log("Mastadon bot starting...");
+// const Mastadon = require('mastodon-api');
+// const fs = require('fs');
+
+// const M = new Mastadon({
+//     client_key: process.env.M_CLIENT_KEY,
+//     client_secret: process.env.M_CLIENT_SECRET,
+//     access_token: process.env.M_AUTH_TOKEN,
+//     timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+//     api_url: 'https://botsin.space/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
+// })
+
+function toot() {
+    const params = {
+        status: "this song came from  my feed on SC\n\n \
+        https://soundcloud.com/gatedrecordings/sets/uf0-im-lost-gtd009\n\n \
+        follow me for more cool EDM tracks on SC:\n\n \
+        https://soundcloud.com/sour_cream_pringles "
+    }
+
+    M.post('statuses', params, (err, data, response) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //reference for data output
+            //fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+            //console.log(data);
+            console.log(`ID: ${data.id} and timestamp: ${data.created_at}`);
+            console.log(data.content);
+            //console.log(response);
+        }
+});
+}
